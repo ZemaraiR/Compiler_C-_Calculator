a32ddf943e3367956ab82d316dfaeb7176f07c8e
ZemaraiR <112522630+ZemaraiR@users.noreply.github.com>
2026-04-24 04:42:50 -0700

    ASTNode change

---
+++ ASTNode.cpp
@@ -0,0 +1,35 @@
+#include "ASTNode.h"
+
+ASTNode::ASTNode(string l) 
+{
+    label = l;
+}
+
+ASTNode::~ASTNode() 
+{
+    for (int i = 0; i < children.size(); i++) delete children[i];
+}
+
+void ASTNode::addChild(ASTNode* child) 
+{
+    if (child != nullptr)
+    {
+        children.push_back(child);
+    } 
+}
+
+void ASTNode::print(string prefix, bool isLast) 
+{
+    cout << prefix;
+    if (isLast) cout << "└── ";
+    else cout << "├── ";
+    cout << label << endl;
+
+    for (int i = 0; i < children.size(); i++) 
+    {
+        string newPrefix = prefix;
+        if (isLast) newPrefix += "    ";
+        else newPrefix += "│   ";
+        children[i]->print(newPrefix, i == children.size() - 1);
+    }
+}
--- ast.h
+++
@@ -1,9 +0,0 @@
-#ifndef AST_H
-#define AST_H
-
-#include <string>
-#include <vector>
-using namespace std;
-
-
-#endif
