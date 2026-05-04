f2b0ce088f81c1af4622ecfb8af17af7a238b98f
ZemaraiR <112522630+ZemaraiR@users.noreply.github.com>
2026-05-03 22:56:23 -0700

    demo 2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 //   g++ -std=c++17 -c token.cpp -o token.o
 //   g++ -std=c++17 -c lexer.cpp -o lexer.o
 //   g++ -std=c++17 -c parser.cpp -o parser.o
-//   g++ -std=c++17 -c ASTNode.cpp -o ASTNode.o
 //   g++ -std=c++17 -c display.cpp -o display.o
 //   g++ -std=c++17 -c main.cpp -o main.o
 //   g++ -o mini_compiler token.o lexer.o parser.o ASTNode.o display.o main.o
